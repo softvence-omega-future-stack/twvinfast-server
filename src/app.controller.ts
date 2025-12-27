a253e5539293328a622a2067903496618462baee
devlopersabbir <devlopersabbir@gmail.com>
2025-12-27 18:41:27 +0600

    change api route for check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Public()
   @Get('api/v1')
   getHelloApi(): string {
-    return this.appService.getHello();
+    return 'Hello from API';
   }
 }
